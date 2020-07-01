57348ab4bf3405d0791313c32ae90e7f9143ede2
thehigestmath <kirill.lfybk.rh@gmail.com>
2020-07-01 09:52:02 +0300

    Вывод сообщений о добавлении/не добавлении ребер. Вывод веса мин. ост. дерева

--- src/main/java/ru/etu/practice/MainWindow.java
+++ src/main/java/ru/etu/practice/MainWindow.java
@@ -65,7 +65,7 @@ public class MainWindow extends JFrame
     public MainWindow() {
         super("Визуализатор алгоритма Краскала");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(900, 640);
+        setSize(950, 640);
         setVisible(true);
 
         pane.setPreferredSize(new Dimension(150, 200));
@@ -86,7 +86,7 @@ public class MainWindow extends JFrame
 //        constraints.ipady = 200;
 //        constraints.ipadx = 200;
         constraints.gridheight = 3;
-        constraints.gridwidth = 2;
+        constraints.gridwidth = 3;
 
         constraints.gridx = 0;  // нулевая ячейка таблицы по вертикали
         constraints.gridy = 0;      // нулевая ячейка таблицы по горизонтали
@@ -96,7 +96,7 @@ public class MainWindow extends JFrame
         constraints.gridx = 0;
         constraints.gridy = 3;
         constraints.ipady = 500;
-        constraints.ipadx = 700;
+        constraints.ipadx = 800;
         constraints.gridheight = 4;
         constraints.gridwidth = 4;
         container.add(graph, constraints);
@@ -105,7 +105,7 @@ public class MainWindow extends JFrame
         constraints.gridx = 4;
         constraints.gridy = 3;
 //        constraints.ipady = 500;
-        constraints.ipadx = 150;
+        constraints.ipadx = 400;
         constraints.gridheight = 4;
         constraints.gridwidth = 1;
         container.add(pane, constraints);
@@ -143,7 +143,7 @@ public class MainWindow extends JFrame
         graph.addMouseListener(this);
         graph.addMouseMotionListener(this);
 
-        Font font = new Font(null, Font.BOLD, 14);
+        Font font = new Font(null, Font.BOLD, 12);
         textArea.setFont(font);
 //
 //        setLocationRelativeTo(null);
@@ -367,6 +367,9 @@ public class MainWindow extends JFrame
                     graphModified = false;
                     graphStep.clearOutput();
                     graph.clearResult();
+                    addStepInfo(String.valueOf(State.CLEAR));
+                    addStepInfo("===============");
+                    actionPerformed(actionEvent);
                 } else {
                     /*
                     do another step
@@ -380,8 +383,9 @@ public class MainWindow extends JFrame
 
                      +Избавиться от проверки stepID < outEdgesStep.size() и заменить её на проверку State
                      */
-                    State tmpState = graphStep.nextStep();
-                    addStepInfo(String.valueOf(tmpState));
+                    List<Object> tuple = graphStep.nextStep();
+                    tuple.add(graphStep.getValue());
+                    addStepInfo(tuple);
                     System.out.println(stepID);
                     if (stepID < outEdgesStep.size()) {
                         Edge edge = outEdgesStep.get(stepID);
@@ -389,8 +393,10 @@ public class MainWindow extends JFrame
                         stepID++;
                         this.graph.repaint();
                     }
-                    if (tmpState == State.END)
+                    if (tuple.get(0) == State.END) {
                         graphModified = true;
+                        addStepInfo("============================");
+                    }
                 }
             } else {
                 /*
@@ -401,6 +407,7 @@ public class MainWindow extends JFrame
                 graphInitiated = true;
                 outEdgesStep = graphStep.getOutputEdges();
                 vertexesStep = graph.getVertexes();
+                actionPerformed(actionEvent);
             }
         } else {
             assert false;
@@ -433,7 +440,39 @@ public class MainWindow extends JFrame
         lines2D.add(line2D);
     }
 
+    private void addStepInfo(List<Object> tuple) {
+        State state = (State) tuple.get(0);
+        StringBuilder addText = new StringBuilder();
+        if (state == State.SORT) {
+            addText.append(state);
+        } else if (state == State.END) {
+            assert tuple.size() > 1;
+            addText.append(state);
+            addText.append("\n");
+            int value = (int) tuple.get(1);
+            addText.append("Minimum spanning tree weight is ");
+            addText.append(value);
+        }
+        else if (state == State.LOOP){
+            assert tuple.size() > 1;
+            addText.append(state);
+            addText.append(" | ");
+            Edge edge = (Edge) tuple.get(1);
+            addText.append(edge);
+            addText.append(" will not be added");
+        }else {
+            assert tuple.size() > 1;
+            addText.append(state);
+            addText.append(" | ");
+            Edge edge = (Edge) tuple.get(1);
+            addText.append(edge);
+            addText.append(" added");
+        }
+        textArea.setText(textArea.getText() + addText.toString() + "\n");
+
+    }
+
     private void addStepInfo(String msg) {
-        textArea.setText(textArea.getText() + msg + '\n');
+        textArea.setText(textArea.getText() + msg + "\n");
     }
 }
